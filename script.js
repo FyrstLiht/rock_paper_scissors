f8ef559a594a0b80591039ff86429cace7c896e2
FyrstLiht <fyrstliht@gmail.com>
2022-04-21 12:08:20 +0100

    Created main layout for program

---
+++ script.js
@@ -0,0 +1,14 @@
+// Declare round, playerWins and cpuWins variables
+// Call playGame
+    // Call playRound
+        // Prompt for player input
+        // Call cpuPlay
+            // Randomly select RPS
+            // Return result
+        // Compare player & cpu results
+        // Return winner
+    // Increment round and win variables
+    // Once round > 5 compare win variables
+    // playGame returns winner
+
+// Functions: playGame, playRound, cpuPlay, playerPrompt
