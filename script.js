b82222e2967bb96b3447908ac3ef374c90517ac8
FyrstLiht <fyrstliht@gmail.com>
2022-04-21 15:49:41 +0100

    Dynamic playthrough, correct user input

--- script.js
+++ script.js
@@ -1,62 +1,71 @@
-console.log(playGame());
+// Starts game
+console.log(`${playGame()} wins the game!`);
 
 function playGame(){
 
-    // Declare round, playerWins and cpuWins variables
-    playerWins = 0,
-    cpuWins = 0;
+    // Declare playerWins and cpuWins variables
+    let playerWins = 0,
+        cpuWins = 0;
 
     // Start 5 round game
     for (let round = 1; round < 6; round++){
-        playRound()
+        console.log(`ROUND ${round}`)
+
+        if (playRound())
+            playerWins++;
+        else
+            cpuWins++;
+
+        // Display score of each round
+        console.log(`PLAYER ${playerWins} - ${cpuWins} CPU\n\n`)
     }
 
-    console.log(playerWins)
-    console.log(cpuWins)
-
-    // True = player win
+    // Return winner
     if (playerWins > cpuWins)
-        return "You win the game";
+        return "PLAYER";
     else
-        return "CPU wins the game";
+        return "CPU";
 }
 
 function playRound(){
-    // Prompt for player input
-    let playerSelection = prompt("Enter selection");
 
-    // Call cpuPlay
-    let cpuSelection = cpuPlay();
+    let playerSelection;
+    const rpsArr = ["Rock", "Paper", "Scissors"];
 
-    console.log(playerSelection)
-    console.log(cpuSelection)
+    // Prompt for correct player input
+    while (true){
+        playerSelection = prompt("Enter selection");
 
-    // Draw
-    if (playerSelection == cpuSelection){
-        console.log("Draw");
-        playRound();
+        // Converts to lowercase and capitalises first letter
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
-    // Win
-    } else if (playerSelection === "rock" && cpuSelection === "scissors"){
-            console.log("You win");
-            playerWins++;
-    } else if (playerSelection === "paper" && cpuSelection === "rock"){
-            console.log("You win");
-            playerWins++;
-    } else if (playerSelection === "scissors" && cpuSelection === "paper"){
-            console.log("You win");
-            playerWins++;
+        // Accepts if a match for correct input
+        if (rpsArr.includes(playerSelection))
+            break;
     }
+
+    // Generates random CPU choice
+    const cpuSelection = rpsArr[Math.floor(Math.random() * 3)];
+
+    // Display player and CPU choices
+    console.log(`PLAYER chooses ${playerSelection}`);
+    console.log(`CPU chooses ${playerSelection}`);
+
+    // Draw, replays round
+    if (playerSelection == cpuSelection){
+        console.log("It's a tie!");
+        playRound();
+    }
+    // Win
+    else if (playerSelection === "Rock" && cpuSelection === "Scissors" 
+            || playerSelection === "Paper" && cpuSelection === "Rock" 
+            || playerSelection === "Scissors" && cpuSelection === "Paper"){
+                console.log("PLAYER wins the round!")
+                return true;
+            }
     // Loss
     else {
-        console.log("You lose");
-        cpuWins++;
-    }
-}
-
-function cpuPlay(){
-
-    const rpsArr = ["rock", "paper", "scissors"];
-    // Return randomly selected RPS
-    return rpsArr[Math.floor(Math.random() * 3)];
+        console.log("CPU wins the round!");
+        return false;
+    }    
 }
