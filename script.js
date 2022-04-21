55303000f2c7fd3d3729e8f1d078bead16882a3e
FyrstLiht <fyrstliht@gmail.com>
2022-04-21 14:34:49 +0100

    Finished basic barebones program

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
-// Declare round, playerWins and cpuWins variables
-// Call playGame
-    // Call playRound
-        // Prompt for player input
-        // Call cpuPlay
-            // Randomly select RPS
-            // Return result
-        // Compare player & cpu results
-        // Return winner
-    // Increment round and win variables
-    // Once round > 5 compare win variables
-    // playGame returns winner
+console.log(playGame());
 
-// Functions: playGame, playRound, cpuPlay, playerPrompt
+function playGame(){
+
+    // Declare round, playerWins and cpuWins variables
+    playerWins = 0,
+    cpuWins = 0;
+
+    // Start 5 round game
+    for (let round = 1; round < 6; round++){
+        playRound()
+    }
+
+    console.log(playerWins)
+    console.log(cpuWins)
+
+    // True = player win
+    if (playerWins > cpuWins)
+        return "You win the game";
+    else
+        return "CPU wins the game";
+}
+
+function playRound(){
+    // Prompt for player input
+    let playerSelection = prompt("Enter selection");
+
+    // Call cpuPlay
+    let cpuSelection = cpuPlay();
+
+    console.log(playerSelection)
+    console.log(cpuSelection)
+
+    // Draw
+    if (playerSelection == cpuSelection){
+        console.log("Draw");
+        playRound();
+
+    // Win
+    } else if (playerSelection === "rock" && cpuSelection === "scissors"){
+            console.log("You win");
+            playerWins++;
+    } else if (playerSelection === "paper" && cpuSelection === "rock"){
+            console.log("You win");
+            playerWins++;
+    } else if (playerSelection === "scissors" && cpuSelection === "paper"){
+            console.log("You win");
+            playerWins++;
+    }
+    // Loss
+    else {
+        console.log("You lose");
+        cpuWins++;
+    }
+}
+
+function cpuPlay(){
+
+    const rpsArr = ["rock", "paper", "scissors"];
+    // Return randomly selected RPS
+    return rpsArr[Math.floor(Math.random() * 3)];
+}
