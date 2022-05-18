e0ab9c34a9b3320d9c3eadea03f074a3c0c5a6e3
fyrstliht <fyrstliht@gmail.com>
2022-05-18 12:06:33 +0100

    Basic html structure and RPS selection screen

--- script.js
+++ script.js
@@ -1,82 +1,47 @@
-// Simulates a Rock Paper Scissors game, first to 3 wins
+// Simulates a ROCK PAPER SCISSORS game, first to 3 wins
 
-console.log("ROCK PAPER SCISSORS\nFirst to 3 wins!\n\n")
-console.log(`${playGame()} wins the game!`);
+// PLAN
 
 
-function playGame(){
 
-    let playerWins = 0,
-        cpuWins = 0,
-        round = 1,
-        roundWinner;
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+let playerWins = 0,
+    cpuWins = 0,
+    winner = '',
+    currentSection = 'splash';
 
-    while (!(playerWins > 2 || cpuWins > 2)){
+updateScore();
+changeSection(currentSection);
 
-        // Round formatting in event of draw
-        if (roundWinner == 0)
-            console.log(`REPLAY ROUND ${round}`)
-        else
-            console.log(`ROUND ${round}`)
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', rpsSelection);
 
-        roundWinner = playRound();
 
-        if (roundWinner == 1){
-            playerWins++;
-            round++;
-        }
-        else if (roundWinner == 2){
-            cpuWins++;
-            round++;
-        }
-        console.log(`PLAYER ${playerWins} - ${cpuWins} CPU\n\n`)
-    }
-    if (playerWins > cpuWins)
-        return "PLAYER";
-    else
-        return "CPU";
+function updateScore() {
+    const playerScore = document.getElementById('playerScore');
+    const cpuScore = document.getElementById('cpuScore');
+    playerScore.textContent = `PLAYER ${playerWins}`;
+    cpuScore.textContent = `${cpuWins} CPU`;
 }
 
-function playRound(){
-
-    const playerSelection = choiceSelection("player");
-    const cpuSelection = choiceSelection("cpu");
-    
-    console.log(`PLAYER chooses ${playerSelection}`);
-    console.log(`CPU chooses ${cpuSelection}`);
-
-    if (playerSelection == cpuSelection){
-        console.log("It's a draw!")
-        return 0;
-    }
-    else if (playerSelection === "Rock" && cpuSelection === "Scissors" 
-            || playerSelection === "Paper" && cpuSelection === "Rock" 
-            || playerSelection === "Scissors" && cpuSelection === "Paper"){
-                console.log("PLAYER wins the round!")
-                return 1;
-            }
-    else {
-        console.log("CPU wins the round!")
-        return 2;
-    }    
+function changeSection(currentSection) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(item => {
+        if (item.id === currentSection)
+            item.classList.remove('hidden')
+        else 
+            item.classList.add('hidden');
+    });
 }
 
-function choiceSelection(selector){
-
-    const choices = ["Rock", "Paper", "Scissors"];
-
-    if (selector === "cpu")
-        // Generates random number that corresponds with choices array
-        return choices[Math.floor(Math.random() * 3)];
-    else {
-        while (true) {
-            playerSelection = prompt("Enter selection");
-            // Formats input into readable format for later use
-            playerSelection = playerSelection.charAt(0).toUpperCase() + 
-            playerSelection.slice(1).toLowerCase();
-
-            if (choices.includes(playerSelection))
-                return playerSelection;
-        }
-    }
+function rpsSelection() {
+    changeSection('rpsSelection');
+    const rpsSelectors = document.querySelectorAll('.rpsSelector');
+    rpsSelectors.forEach(item => item.addEventListener('mouseenter', hoverChoice));
+    rpsSelectors.forEach(item => item.addEventListener('mouseleave', hoverChoice));
 }
+
+function hoverChoice(event) {
+    this.classList.toggle('hoverChoice');
+}
+
