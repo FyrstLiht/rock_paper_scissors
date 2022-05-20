3204d46e11ee369b1f977f43eac5b2b4c47308f8
fyrstliht <fyrstliht@gmail.com>
2022-05-20 21:00:11 +0100

    Basic playthrough with new UI

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Simulates a ROCK PAPER SCISSORS game, first to 3 wins
+// Simulates a rock paper scissors game, first to 3 wins
 
-const choices = ["ROCK", "PAPER", "SCISSORS"];
+const choices = ["rock", "paper", "scissors"];
 let playerWins = 0,
     cpuWins = 0,
     winner = '',
@@ -10,35 +10,23 @@ let playerWins = 0,
     cpuColour = 'blue',
     currentSection = 'splash';
 
-// updateScore();
-// changeSection(currentSection);
-
-const startButton = document.getElementById('startButton');
-const menuButtons = document.querySelectorAll('.menuButtons');
-// const backToSplash = document.getElementById('backToSplash');
-
-startButton.addEventListener('click', playGame);
-// backToSplash.addEventListener('click', changeSection);
-menuButtons.forEach(item => {
-    item.addEventListener('click', toggleMenus);
-});
+setButtons();
 
 
-// function updateScore() {
-//     const playerScore = document.getElementById('playerScore');
-//     const cpuScore = document.getElementById('cpuScore');
-//     playerScore.textContent = `PLAYER ${playerWins}`;
-//     cpuScore.textContent = `${cpuWins} CPU`;
-// }
 
-function changeSection() {
-    const sections = document.querySelectorAll('.container');
-    sections.forEach(item => {
-        if (item.classList.contains('hidden'))
-            item.classList.remove('hidden')
-        else 
-            item.classList.add('hidden');
+
+function setButtons() {
+    const startButton = document.getElementById('startButton');
+    const menuButtons = document.querySelectorAll('.menuButtons');
+    const rpsSelectors = document.querySelectorAll('.rpsSelector');
+    const leftButton = document.getElementById('leftButton');
+
+    rpsSelectors.forEach(item => item.addEventListener('click', playRound));
+    startButton.addEventListener('click', startGame);
+    menuButtons.forEach(item => {
+        item.addEventListener('click', toggleMenus);
     });
+    leftButton.addEventListener('click', newRound);
 }
 
 function toggleMenus() {
@@ -51,81 +39,96 @@ function toggleMenus() {
     });
 }
 
-function playGame() {
+function startGame() {
     changeSection();
+    updateHUD();
+    // loadHands();
+    toggleHide('rpsSelection');
+}
+
+function changeSection() {
+    const sections = document.querySelectorAll('.container');
+    sections.forEach(item => {
+        if (item.classList.contains('hidden'))
+            item.classList.remove('hidden')
+        else 
+            item.classList.add('hidden');
+    });
+}
+
+function updateHUD() {
+    const playerScore = document.getElementById('playerScore');
+    const cpuScore = document.getElementById('cpuScore');
+    playerScore.textContent = `PLAYER ${playerWins}`;
+    cpuScore.textContent = `${cpuWins} CPU`;
+}
+
+function loadHands() {
+
+}
+
+function toggleHide(toHide) {
+    const hideThis = document.getElementById(toHide);
+    hideThis.classList.toggle('hidden');
+}
+
+function playRound(event) {
+    toggleHide('rpsSelection');
+    getResult(event);
+    gameplay();
+    displayResult();
+}
+
+function getResult(event) {
+    let playerSelection = event.target.id;
+    let cpuSelection = choices[Math.floor(Math.random() * 3)];
+
+    if (playerSelection === cpuSelection) {
+        winner = 'Draw';
+    }
+    else if (playerSelection === "rock" && cpuSelection === "scissors" 
+        || playerSelection === "paper" && cpuSelection === "rock" 
+        || playerSelection === "scissors" && cpuSelection === "paper"){
+        winner = 'PLAYER';
+        playerWins++;
+    }
+    else {
+        winner = 'CPU';
+        cpuWins++;
+    }
+}
+
+function gameplay() {
+
+}
+
+function displayResult() {
+    toggleHide('displayResult');
+    const resultText = document.getElementById('result');
+    
+    if (playerWins === 3 || cpuWins === 3){
+        resultText.textContent = `${winner} wins the game!`;
+        const leftButton = getElementById('leftButton');
+        leftButton.textContent = 'MAIN MENU';
+        leftButton.addEventListener('click', changeSection); 
+    } else {
+        if (winner === 'Draw')
+            resultText.textContent = 'Draw!';
+        else
+            resultText.textContent = `${winner} wins the round!`;
+    }
+    updateHUD();
+}
+
+function newRound() {
+    toggleHide('displayResult');
+    toggleHide('rpsSelection');
 }
 
 
 
 
-// function rpsSelection() {
-//     changeSection('rpsSelection');
-//     const rpsSelectors = document.querySelectorAll('.rpsSelector');
-//     rpsSelectors.forEach(item => item.addEventListener('click', playRound));
-//     rpsSelectors.forEach(item => item.addEventListener('mouseenter', hoverChoice));
-//     rpsSelectors.forEach(item => item.addEventListener('mouseleave', hoverChoice));
-// }
 
-// function hoverChoice(event) {
-//     this.classList.toggle('hoverChoice');
-// }
-
-// function playRound(event) {
-    
-//     let playerSelection = event.target.id;
-//     let cpuSelection = choices[Math.floor(Math.random() * 3)];
-
-//     const battleSelections = document.querySelector('.battleSelections');
-//     battleSelections.textContent = `${playerSelection} VS ${cpuSelection}`;
-//     battleSelections.textContent.toUpperCase();
-
-//     if (playerSelection === cpuSelection) {
-//         winner = 'Draw';
-//     }
-//     else if (playerSelection === "ROCK" && cpuSelection === "SCISSORS" 
-//         || playerSelection === "PAPER" && cpuSelection === "ROCK" 
-//         || playerSelection === "SCISSORS" && cpuSelection === "PAPER"){
-//         winner = 'PLAYER';
-//         playerWins++;
-//     }
-//     else {
-//         winner = 'CPU';
-//         cpuWins++;
-//     }
-//     updateScore();
-//     displayResult()
-//     doBattle();
-// }
-
-// function displayResult() {
-//     const resultText = document.querySelector('.resultText');
-//     if (playerWins === 3 || cpuWins === 3)
-//         resultText.textContent = `${winner} wins the game!`;
-//     else {
-//         if (winner === 'Draw')
-//             resultText.textContent = 'Draw!';
-//         else
-//             resultText.textContent = `${winner} wins the round!`;
-//     }
-// }
-
-// function doBattle() {
-//     changeSection('battle');
-//     setTimeout(battleResult, 2000);
-// }
-
-// function battleResult() {
-//     changeSection('battleResult');
-//     const newRound = document.getElementById('newRound');
-//     if (playerWins === 3 || cpuWins === 3){
-//         newRound.textContent = 'NEW GAME';
-//         newRound.addEventListener('click', startNewGame);
-//     }
-//     else {
-//         newRound.textContent = 'NEW ROUND'
-//         newRound.addEventListener('click', rpsSelection);
-//     }
-// }
 
 // function startNewGame() {
 //     playerWins = 0,
@@ -134,6 +137,8 @@ function playGame() {
 //     newRound.textContent = 'NEW ROUND'
 //     newRound.addEventListener('click', rpsSelection);
 //     newRound.removeEventListener('click', startNewGame);
-//     updateScore();
+//     updateHUD();
 //     rpsSelection();
 // }
+
+//     setTimeout(battleResult, 2000);
