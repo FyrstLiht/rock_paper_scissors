08cd452976e8f09e69b838b9c574ae8957e294f7
fyrstliht <fyrstliht@gmail.com>
2022-05-20 10:38:23 +0100

    Dynamic splash menu functionality

--- script.js
+++ script.js
@@ -1,48 +1,137 @@
 // Simulates a ROCK PAPER SCISSORS game, first to 3 wins
 
-// TODO
-// Colour selector stand in
-// Center selection, absolute continue
-// Gameplay screen with HUD
-// Absolutely positioned rps selection screen
-
-
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 let playerWins = 0,
     cpuWins = 0,
     winner = '',
+    playerHand = 'left',
+    cpuHand = 'right',
+    playerColour = 'red',
+    cpuColour = 'blue',
     currentSection = 'splash';
 
-updateScore();
-changeSection(currentSection);
+// updateScore();
+// changeSection(currentSection);
 
 const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', handSelection);
+const menuButtons = document.querySelectorAll('.menuButtons');
+
+startButton.addEventListener('click', playGame);
+menuButtons.forEach(item => {
+    item.addEventListener('click', toggleMenus);
+});
 
 
-function updateScore() {
-    const playerScore = document.getElementById('playerScore');
-    const cpuScore = document.getElementById('cpuScore');
-    playerScore.textContent = `PLAYER ${playerWins}`;
-    cpuScore.textContent = `${cpuWins} CPU`;
-}
+// function updateScore() {
+//     const playerScore = document.getElementById('playerScore');
+//     const cpuScore = document.getElementById('cpuScore');
+//     playerScore.textContent = `PLAYER ${playerWins}`;
+//     cpuScore.textContent = `${cpuWins} CPU`;
+// }
 
-function changeSection(currentSection) {
+function changeSection() {
     const sections = document.querySelectorAll('.container');
     sections.forEach(item => {
-        if (item.classList.contains(currentSection))
+        if (item.classList.contains('hidden'))
             item.classList.remove('hidden')
         else 
             item.classList.add('hidden');
     });
 }
 
-function handSelection() {
-    changeSection('handSelection');
-    const continueGame = document.getElementById('continueGame');
-    // continueGame.addEventListener('click', rpsSelection);
+function toggleMenus() {
+    const menuType = this.classList[0];
+    const menus = document.querySelectorAll('.menu');
+    menus.forEach(menu => {
+        if (menu.classList.contains(menuType)){
+            menu.classList.toggle('hidden');
+        }
+    });
+}
+
+function playGame() {
+    changeSection();
 }
 
 
 
 
+// function rpsSelection() {
+//     changeSection('rpsSelection');
+//     const rpsSelectors = document.querySelectorAll('.rpsSelector');
+//     rpsSelectors.forEach(item => item.addEventListener('click', playRound));
+//     rpsSelectors.forEach(item => item.addEventListener('mouseenter', hoverChoice));
+//     rpsSelectors.forEach(item => item.addEventListener('mouseleave', hoverChoice));
+// }
+
+// function hoverChoice(event) {
+//     this.classList.toggle('hoverChoice');
+// }
+
+// function playRound(event) {
+    
+//     let playerSelection = event.target.id;
+//     let cpuSelection = choices[Math.floor(Math.random() * 3)];
+
+//     const battleSelections = document.querySelector('.battleSelections');
+//     battleSelections.textContent = `${playerSelection} VS ${cpuSelection}`;
+//     battleSelections.textContent.toUpperCase();
+
+//     if (playerSelection === cpuSelection) {
+//         winner = 'Draw';
+//     }
+//     else if (playerSelection === "ROCK" && cpuSelection === "SCISSORS" 
+//         || playerSelection === "PAPER" && cpuSelection === "ROCK" 
+//         || playerSelection === "SCISSORS" && cpuSelection === "PAPER"){
+//         winner = 'PLAYER';
+//         playerWins++;
+//     }
+//     else {
+//         winner = 'CPU';
+//         cpuWins++;
+//     }
+//     updateScore();
+//     displayResult()
+//     doBattle();
+// }
+
+// function displayResult() {
+//     const resultText = document.querySelector('.resultText');
+//     if (playerWins === 3 || cpuWins === 3)
+//         resultText.textContent = `${winner} wins the game!`;
+//     else {
+//         if (winner === 'Draw')
+//             resultText.textContent = 'Draw!';
+//         else
+//             resultText.textContent = `${winner} wins the round!`;
+//     }
+// }
+
+// function doBattle() {
+//     changeSection('battle');
+//     setTimeout(battleResult, 2000);
+// }
+
+// function battleResult() {
+//     changeSection('battleResult');
+//     const newRound = document.getElementById('newRound');
+//     if (playerWins === 3 || cpuWins === 3){
+//         newRound.textContent = 'NEW GAME';
+//         newRound.addEventListener('click', startNewGame);
+//     }
+//     else {
+//         newRound.textContent = 'NEW ROUND'
+//         newRound.addEventListener('click', rpsSelection);
+//     }
+// }
+
+// function startNewGame() {
+//     playerWins = 0,
+//     cpuWins = 0;
+//     const newRound = document.getElementById('newRound');
+//     newRound.textContent = 'NEW ROUND'
+//     newRound.addEventListener('click', rpsSelection);
+//     newRound.removeEventListener('click', startNewGame);
+//     updateScore();
+//     rpsSelection();
+// }
