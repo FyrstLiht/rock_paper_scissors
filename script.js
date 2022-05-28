3dcb4bfad9008d166214136244e1467fd4e93d85
fyrstliht <fyrstliht@gmail.com>
2022-05-28 19:42:40 +0100

    Added draw animation and show RPS choices

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 const choices = ["rock", "paper", "scissors"];
 const playerArr = ["playerShoot", ""];
 const cpuArr = ["cpuShoot", ""];
-const animTimings = [10, 1300, 2500];
+const animTimings = [10, 1500, 2500];
 let playerWins = 0,
     cpuWins = 0,
     winner = '',
     loser = '',
     playerHand = 'left',
     playerColour = 'red',
-    cpuColour = 'blue';
+    cpuColour = 'blue'
+    playerSelection = '',
+    cpuSelection = '';
 
 setButtons();
 startSplash();
@@ -26,6 +28,7 @@ function startGame() {
     toggleHide('mainGame', 'show');
     toggleHide('displayResult', 'hide');
     toggleHide('resultButtons', 'hide');
+
     resetResults();
     loadHands();
     setTimeout( () => {
@@ -38,12 +41,23 @@ function newRound(event) {
     getResult(event);
     animateHands('playerHand', playerArr.slice());
     animateHands('cpuHand', cpuArr.slice());
+    setTimeout(showRPS, 900);
     setTimeout(displayResult, 2500);
 }
 
+function loadHands() {
+    const hands = document.querySelectorAll('.hands');
+    hands.forEach(item => {
+        item.classList.remove('hidden');
+    });
+    hands[0].textContent = `${playerHand} hand`;
+    if (playerHand === 'left') hands[1].textContent = 'right hand';
+    else hands[1].textContent = 'left hand';
+}
+
 function getResult(event) {
-    let playerSelection = event.target.id;
-    let cpuSelection = choices[Math.floor(Math.random() * 3)];
+    playerSelection = event.target.id;
+    cpuSelection = choices[Math.floor(Math.random() * 3)];
 
     if (playerSelection === cpuSelection) {
         winner = 'draw';
@@ -66,7 +80,7 @@ function animateHands(handID, array) {
     const currentHand = document.getElementById(handID);
     let i = 0;
     if (winner === 'draw') {
-        array[1] = `${handID.replace("Hand", "")}Win`;
+        array[1] = `${handID.replace("Hand", "")}Draw`;
     } else if (winner === handID.replace("Hand", "")){
         array[1] = `${winner}Win`;
     } else {
@@ -105,6 +119,18 @@ function displayResult() {
     }
 }
 
+function showRPS() {
+    const hands = document.querySelectorAll('.hands');
+    hands[0].textContent = `${playerSelection}`;
+    hands[1].textContent = `${cpuSelection}`;
+
+    setTimeout( () => {
+        hands[0].textContent = `${playerHand} hand`;
+        if (playerHand === 'left') hands[1].textContent = 'right hand';
+        else hands[1].textContent = 'left hand';
+    }, 3000);
+}
+
 function setButtons() {
     const startButton = document.getElementById('startButton');
     const newGame = document.getElementById('newGame');
@@ -125,16 +151,6 @@ function setButtons() {
     mainMenu.addEventListener('click', startSplash);
     leftHandSelect.addEventListener('click', switchHands);
     rightHandSelect.addEventListener('click', switchHands);
-}
-
-function loadHands() {
-    const hands = document.querySelectorAll('.hands');
-    hands.forEach(item => {
-        item.classList.remove('hidden');
-    });
-    hands[0].textContent = `${playerHand} hand`;
-    if (playerHand === 'left') hands[1].textContent = 'right hand';
-    else hands[1].textContent = 'left hand';
 }
 
 function switchHands(event) {
